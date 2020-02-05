74a7e44842db290d7836232ef289a6d41176ef2f
Lillian Luzinsky <eyeris@tpg.com.au>
2020-02-06 07:39:33 +1100

    finished sortable tutorial

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Gif from './gif';
 const SortableGifsContainer = sortableContainer(({ children }) => <div className="gifs">{children}</div>);
 const SortableGif = sortableElement(({ gif }) => <Gif key={gif} gif={gif} />);
 
+
 const App = () => {
   const [gifs, setGifs] = useState([
     'https://media.giphy.com/media/3ohhwoWSCtJzznXbuo/giphy.gif',
@@ -15,19 +16,46 @@ const App = () => {
     'https://media.giphy.com/media/xT1XGYy9NPhWRPp4pq/giphy.gif',
   ]);
 
-  const onSortEnd = ({ oldIndex, newIndex }) => setGifs(arrayMove(gifs, oldIndex, newIndex));
+  const [newGifs, setNewGifs] = useState([
+    'https://media.giphy.com/media/xiOgHgY2ceKhm46cAj/giphy.gif',
+    'https://media.giphy.com/media/3oKIPuMqYfRsyJTWfu/giphy.gif',
+    'https://media.giphy.com/media/4ZgLPakqTajjVFOVqw/giphy.gif',
+    'https://media.giphy.com/media/3o7btXIelzs8nBnznG/giphy.gif',
+  ]);
+
+  const onSortEnd = ({ oldIndex, newIndex, collection }) => {
+    switch (collection) {
+      case 'gifs':
+        setGifs(arrayMove(gifs, oldIndex, newIndex))
+        break;
+      case 'newGifs':
+        setNewGifs(arrayMove(newGifs, oldIndex, newIndex))
+        break;
+      default:
+        break;
+    }
+  }
 
   return (
-    <SortableGifsContainer axis="x" onSortEnd={onSortEnd}>
-      {gifs.map((gif, i) =>
-        <SortableGif
-          // don't forget to pass index prop with item index
-          index={i}
-          key={gif}
-          gif={gif}
-        />
-      )}
-    </SortableGifsContainer>
+    <div className="App">
+      <h1>Drag those gifs around</h1>
+      <h2>Set 1</h2>
+      <SortableGifsContainer axis="x" onSortEnd={onSortEnd} onSortStart={(_, event) => event.preventDefault()}>
+        {gifs.map((gif, i) =>
+          <SortableGif
+            index={i}
+            key={gif}
+            gif={gif}
+            collection="gifs"
+          />
+        )}
+      </SortableGifsContainer>
+
+      <h2>Set 2</h2>
+      <SortableGifsContainer axis="x" onSortEnd={onSortEnd} onSortStart={(_, event) => event.preventDefault()} >
+        {newGifs.map((gif, i) => <SortableGif index={i} key={gif} gif={gif} collection="newGifs" />)}
+      </SortableGifsContainer>
+    </div>
   );
 }
 
